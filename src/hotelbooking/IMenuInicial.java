2b7f806c3d7ea732822ac7f888c10085e81ba7ff
kelvin <urielbecerra39@gmail.com>
2024-03-27 22:52:40 -0400

    centrando

--- src/hotelbooking/IMenuInicial.java
+++ src/hotelbooking/IMenuInicial.java
@@ -297,6 +297,8 @@ public class IMenuInicial extends javax.swing.JFrame {
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
         IMenuCheckOut mco = new IMenuCheckOut(clientes, reservas, habs);
+        mco.setLocationRelativeTo(null);
+
         this.dispose();
     }//GEN-LAST:event_jButton1ActionPerformed
 
